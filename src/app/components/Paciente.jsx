acee20df16fd6d4cdbdec79d0e35cc4fc79a9b33
marcel zuniga <alejocrak321zuniga@gmail.com>
2025-06-07 17:21:10 -0600

    actualizacion mas reciente

--- src/app/components/Paciente.jsx
+++ src/app/components/Paciente.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import '../style/Paciente.css'
 
 export  function Paciente() {
   return (
-    <div>Paciente</div>
+    <div className='div-father'>
+    <div className='div-left'>
+      left
+    </div>
+    <div className='div-right'>
+      <div className='right-top'>
+        top
+      </div>
+      <div className='right-bottom'>
+        bottom
+      </div>
+    </div>
+    </div>
   )
 }
